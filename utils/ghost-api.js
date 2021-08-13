9122b1770340acf4d49dad2ebf2683a15c96c1ed
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-08-13 16:22:57 +0900

    feat: add version key to ghost api setup, allow nullish coalescing in eslint

--- utils/ghost-api.js
+++ utils/ghost-api.js
@@ -1,19 +1,20 @@
-const ghostContentAPI = require("@tryghost/content-api");
+const ghostContentAPI = require('@tryghost/content-api');
+const ghostVersion = process.env.GHOST_API_VERSION ?? 'v3';
 
 // Init Ghost API
 const api = new ghostContentAPI({
   url: process.env.GHOST_API_URL,
   key: process.env.GHOST_CONTENT_API_KEY,
-  version: "v3"
+  version: ghostVersion,
 });
 
 const enApi = new ghostContentAPI({
   url: process.env.EN_GHOST_API_URL,
   key: process.env.EN_GHOST_CONTENT_API_KEY,
-  version: "v3"
+  version: ghostVersion,
 });
 
 module.exports = {
   api,
-  enApi
+  enApi,
 };
