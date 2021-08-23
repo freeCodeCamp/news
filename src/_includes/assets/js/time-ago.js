7a53c50eeeb1a786f034fb30ce00b59d94fba690
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-08-23 18:49:58 +0900

    fix: client time-ago selector

--- src/_includes/assets/js/time-ago.js
+++ src/_includes/assets/js/time-ago.js
@@ -1,11 +1,11 @@
-/* eslint-disable no-undef */
 document.addEventListener('DOMContentLoaded', () => {
-  const postDates = [...document.querySelectorAll('.post-card-meta > .meta-content > time.meta-item')];
+  const postDates = [...document.querySelectorAll('.post-card-meta .meta-content > time.meta-item')];
 
   postDates.forEach(date => {
     const dateStr = date.getAttribute('datetime');
 
     // Display time ago date
+    // eslint-disable-next-line no-undef
     date.innerHTML = dayjs().to(dayjs(dateStr));
   });
 });
