8906ea29372e655600e61de0b921f40723f943ed
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-08-24 13:25:04 +0900

    feat: get Ghost API keys dynamically

--- utils/ghost-api.js
+++ utils/ghost-api.js
@@ -1,39 +1,27 @@
 const ghostContentApi = require('@tryghost/content-api');
 const ghostApiSource = process.env.GHOST_API_SOURCE;
-const keys = {
-  local: {
-    url: process.env.LOCAL_GHOST_API_URL,
-    key: process.env.LOCAL_GHOST_CONTENT_API_KEY,
-    version: process.env.LOCAL_GHOST_API_VERSION
-  },
-  en: {
-    url: process.env.EN_GHOST_API_URL,
-    key: process.env.EN_GHOST_CONTENT_API_KEY,
-    version: process.env.EN_GHOST_API_VERSION,
-  },
-  zh: {
-    url: process.env.ZH_GHOST_API_URL,
-    key: process.env.ZH_GHOST_CONTENT_API_KEY,
-    version: process.env.ZH_GHOST_API_VERSION,
-  },
-  es: {
-    url: process.env.ES_GHOST_API_URL,
-    key: process.env.ES_GHOST_CONTENT_API_KEY,
-    version: process.env.ES_GHOST_API_VERSION,
+
+const fetchKeys = (locale) => {
+  const upperLocale = locale.toUpperCase();
+
+  return {
+    url: process.env[`${upperLocale}_GHOST_API_URL`],
+    key: process.env[`${upperLocale}_GHOST_CONTENT_API_KEY`],
+    version: process.env[`${upperLocale}_GHOST_API_VERSION`]
   }
-};
+}
 
 // Init Ghost API
-const api = new ghostContentApi(keys[ghostApiSource]);
+const api = new ghostContentApi(fetchKeys(ghostApiSource));
 
 // For the translator / original author citation feature
 // To do: Refactor this to work for any language using the keys above
-const enApi = new ghostContentApi(keys['en']);
+const enApi = new ghostContentApi(fetchKeys('en'));
 
 // Export API instances and target API URL for link swapping,
 // fetching sitemaps, etc.
 module.exports = {
   api,
   enApi,
-  apiUrl: keys[ghostApiSource].url
+  apiUrl: fetchKeys(ghostApiSource).url
 };
