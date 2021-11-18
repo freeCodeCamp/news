856d46c4780cf7be5d1d9a6171b745d251a7bf69
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-11-18 19:11:37 +0900

    feat: basic cypress setup

---
+++ cypress-install.sh
@@ -0,0 +1,16 @@
+InstallCypress() {
+    sudo apt update && sudo apt install -y \
+        libgtk2.0-0 \
+        libgtk-3-0 \
+        libgbm-dev \
+        libnotify-dev \
+        libgconf-2-4 \
+        libnss3 \
+        libxss1 \
+        libasound2 \
+        libxtst6 \
+        xauth \
+        xvfb
+}
+
+InstallCypress
