416ad7e888fa74579cd9df868ee738a6c76fa066
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-11-23 15:56:38 +0900

    fix: dynamic dayjs locale

--- src/_includes/assets/js/client-dayjs.js
+++ src/_includes/assets/js/client-dayjs.js
@@ -3,5 +3,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // Load dayjs plugins and set locale
   dayjs.extend(dayjs_plugin_localizedFormat);
   dayjs.extend(dayjs_plugin_relativeTime);
-  dayjs.locale('{{ site.lang }}');
+  dayjs.locale('{{ site.lang | lower }}');
 });
