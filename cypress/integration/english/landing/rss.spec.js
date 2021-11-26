411b1d8414112b76edf78173fd2ec79efc592fc1
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-11-26 20:13:25 +0900

    feat: add simple rss tests

---
+++ cypress/integration/english/landing/rss.spec.js
@@ -0,0 +1,24 @@
+describe('Landing RSS feed', () => {
+  let feed;
+
+  before(async () => {
+    // To do: improve RSS tests by using an actual XML parser to
+    // select and test specific fields (titles, descriptions,
+    // timestamps, etc.)
+    const parser = new DOMParser();
+    const res = await cy.request('/rss.xml');
+    feed = parser.parseFromString(res.body, 'application/xml');
+  });
+
+  it('should start with the title <![CDATA[ freeCodeCamp.org ]]>', () => {
+    const title = feed.querySelector('channel title').innerHTML.trim();
+
+    expect(title).to.equal('<![CDATA[ freeCodeCamp.org ]]>');
+  });
+
+  it('should return 10 articles', () => {
+    const articles = feed.querySelectorAll('item');
+
+    expect([...articles]).to.have.lengthOf(10);
+  });
+});
