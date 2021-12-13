0b3f7f1dedaa93e5a4fce584ad72bef81027f8cf
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-12-13 22:25:52 +0900

    fix: add common stuctured data fixture

--- cypress/integration/english/landing/structured-data.spec.js
+++ cypress/integration/english/landing/structured-data.spec.js
@@ -1,32 +1,6 @@
 describe('Landing structured data (JSON-LD)', () => {
   let jsonLdObj;
-  const jsonLdExpected = {
-    '@context': 'https://schema.org',
-    '@type': 'WebSite',
-    publisher: {
-      '@type': 'Organization',
-      name: 'freeCodeCamp.org',
-      url: 'http://localhost:8080/news/',
-      logo: {
-        '@type': 'ImageObject',
-        url: 'https://cdn.freecodecamp.org/platform/universal/fcc_primary.svg',
-        width: 2100,
-        height: 240
-      }
-    },
-    image: {
-      '@type': 'ImageObject',
-      url: 'https://cdn.freecodecamp.org/platform/universal/fcc_meta_1920X1080-indigo.png',
-      width: 1920,
-      height: 1080
-    },
-    url: 'http://localhost:8080/news/',
-    mainEntityOfPage: {
-      '@type': 'WebPage',
-      '@id': 'http://localhost:8080/news/'
-    },
-    description: 'Browse thousands of programming tutorials written by experts. Learn Web Development, Data Science, DevOps, Security, and get developer career advice.'
-  }
+  const commonStructuredData = require('../../../fixtures/common-structured-data.json');
 
   before(() => {
     cy.visit('/');
@@ -37,21 +11,21 @@ describe('Landing structured data (JSON-LD)', () => {
   });
 
   it('matches the expected base values', () => {
-    expect(jsonLdObj['@context']).to.equal(jsonLdExpected['@context']);
-    expect(jsonLdObj['@type']).to.equal(jsonLdExpected['@type']);
-    expect(jsonLdObj.url).to.equal(jsonLdExpected.url);
-    expect(jsonLdObj.description).to.equal(jsonLdExpected.description);
+    expect(jsonLdObj['@context']).to.equal(commonStructuredData['@context']);
+    expect(jsonLdObj['@type']).to.equal(commonStructuredData['@type']);
+    expect(jsonLdObj.url).to.equal(commonStructuredData.url);
+    expect(jsonLdObj.description).to.equal(commonStructuredData.description);
   });
 
   it('matches the expected publisher values', () => {
-    expect(jsonLdObj.publisher).to.deep.equal(jsonLdExpected.publisher);
+    expect(jsonLdObj.publisher).to.deep.equal(commonStructuredData.publisher);
   });
 
   it('matches the expected image values', () => {
-    expect(jsonLdObj.image).to.deep.equal(jsonLdExpected.image);
+    expect(jsonLdObj.image).to.deep.equal(commonStructuredData.image);
   });
 
   it('matches the expected mainEntityOfPage values', () => {
-    expect(jsonLdObj.mainEntityOfPage).to.deep.equal(jsonLdExpected.mainEntityOfPage);
+    expect(jsonLdObj.mainEntityOfPage).to.deep.equal(commonStructuredData.mainEntityOfPage);
   });
 });
