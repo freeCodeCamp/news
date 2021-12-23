f797fc21d65a47691e7e61c11b910f5336899fe8
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-12-23 19:41:21 +0900

    fix: move default tweet full stop into translation.json files

--- src/_includes/assets/js/social-row.js
+++ src/_includes/assets/js/social-row.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const title = encodeURIComponent('{{ post.title | escape }}');
   const twitter = '{{ post.primary_author.twitter }}';
   const url = window.location;
-  const thanks = `{% t 'social-row.default-tweet', { twitter: post.primary_author.twitter } %}.` +
+  const thanks = `{% t 'social-row.default-tweet', { twitter: post.primary_author.twitter } %}` +
       `%0A%0A${title}%0A%0A${url}`;
   const button = document.getElementById('tweet-btn');
   button.addEventListener('click', () => {
