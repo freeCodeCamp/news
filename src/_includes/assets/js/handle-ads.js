0776b26ded4fa505a9b86fef16e4deded5d0858d
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-12-30 20:10:09 +0900

    fix: prevent handle-ads script from being added to page when it shouldn't be

--- src/_includes/assets/js/handle-ads.js
+++ src/_includes/assets/js/handle-ads.js
@@ -1,24 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const adsEnabled = '{{ secrets.adsEnabled }}' === 'true';
+  // May not be necessary
+  document.cookie = 'reader=1; SameSite=None; Secure';
+  const sidebar = document.querySelector('.sidebar');
+  const bannerAd = document.querySelector('.banner-ad');
+  const postAndSidebar = document.querySelector('.post-and-sidebar');
+  const postFullContent = document.querySelector('.post-full-content');
+  const notAuthenticated = !document.cookie.split(';')
+    .some((item) => item.trim()
+    .startsWith('jwt_access_token='));
 
-  if (adsEnabled) {
-    // May not be necessary
-    document.cookie = 'reader=1; SameSite=None; Secure';
-    const sidebar = document.querySelector('.sidebar');
-    const bannerAd = document.querySelector('.banner-ad');
-    const postAndSidebar = document.querySelector('.post-and-sidebar');
-    const postFullContent = document.querySelector('.post-full-content');
-    const notAuthenticated = !document.cookie.split(';')
-      .some((item) => item.trim()
-      .startsWith('jwt_access_token='));
+  if (notAuthenticated) {
+    sidebar.style.display = 'flex';
+    bannerAd.style.display = 'block';
 
-    if (notAuthenticated) {
-      sidebar.style.display = 'flex';
-      bannerAd.style.display = 'block';
-
-      // These changes may be too noticeable
-      postAndSidebar.style.display = 'grid';
-      postFullContent.style.padding = '70px 0 0';
-    }
+    // These changes may be too noticeable
+    postAndSidebar.style.display = 'grid';
+    postFullContent.style.padding = '70px 0 0';
   }
 });
