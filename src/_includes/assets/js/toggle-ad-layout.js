f8c3b937c789fae272d73063c4a2fd60ef5c6252
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-01-03 19:18:44 +0900

    fix: loading ads (#116)

--- src/_includes/assets/js/toggle-ad-layout.js
+++ src/_includes/assets/js/toggle-ad-layout.js
@@ -1,20 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   // May not be necessary
   document.cookie = 'reader=1; SameSite=None; Secure';
-  const sidebar = document.querySelector('.sidebar');
-  const bannerAd = document.querySelector('.banner-ad');
-  const postAndSidebar = document.querySelector('.post-and-sidebar');
-  const postFullContent = document.querySelector('.post-full-content');
+  const inner = document.querySelector('.inner');
   const notAuthenticated = !document.cookie.split(';')
     .some((item) => item.trim()
     .startsWith('jwt_access_token='));
 
   if (notAuthenticated) {
-    sidebar.style.display = 'flex';
-    bannerAd.style.display = 'block';
-
-    // These changes may be too noticeable
-    postAndSidebar.style.display = 'grid';
-    postFullContent.style.padding = '70px 0 0';
+    inner.classList.add('ad-layout');
   }
 });
