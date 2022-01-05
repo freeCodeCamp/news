1f7eb89d0ea55fd84897d5219adfae432e08305e
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-01-06 00:26:36 +0900

    fix: decouple author translator feature and CI build from dev / prod builds (#129)

--- utils/ghost/original-post-handler.js
+++ utils/ghost/original-post-handler.js
@@ -1,36 +1,66 @@
-const { englishApi } = require('./api');
+const ghostContentAPI = require('@tryghost/content-api');
+const { URL } = require('url');
+const { fetchKeys } = require('./api');
+const { locales } = require('../../config');
+
+const ghostURLToAPIMap = locales.reduce((obj, currLocale) => {
+  const { url, key, version } = fetchKeys(currLocale);
+
+  try {
+    obj[url] = {
+      api: new ghostContentAPI({ url, key, version }),
+      locale_i18n: currLocale
+    }
+  } catch(err) {
+    console.warn(`
+      ---------------------------------------------------------------
+      Warning: Unable to initialize the Content API for ${currLocale}
+      ---------------------------------------------------------------
+      Please double check that the correct keys are included in the
+      .env file.
+      You can ignore this warning if this instance of Ghost is set 
+      to private, if you don't need the original / author translator
+      feature for this locale, or if a test suite is running.
+      ---------------------------------------------------------------
+    `);
+  }
+
+  return obj;
+}, {});
 
 const originalPostHandler = async (post) => {
   const originalPostRegex = /const\s+fccOriginalPost\s+=\s+("|')(?<url>.*)\1;?/g;
   const match = originalPostRegex.exec(post.codeinjection_head);
   
   if (match) {
-    const url = match.groups.url;
-    const urlArr = url.split('/');
-    // handle urls that end with a slash,
-    // then urls that don't end in a slash
-    const originalPostSlug = urlArr[urlArr.length - 1] ?
-      urlArr[urlArr.length - 1] :
-      urlArr[urlArr.length - 2];
-    const originalPostRes = await englishApi.posts
-      .read({
-        include: 'authors',
-        slug: originalPostSlug
-      })
-      .catch(err => {
-        console.error(err);
-      });
-    const {
-      title,
-      published_at,
-      primary_author
-    } = originalPostRes;
+    try {
+      const { origin, pathname } = new URL(match.groups.url);
+      let pathSegments = pathname.split('/').filter((str) => str);
+      const originalPostSlug = pathSegments.pop();
+      const originalPostGhostURL = `${origin}/${pathSegments.join('/')}`;
+      const { api, locale_i18n } = ghostURLToAPIMap[originalPostGhostURL];
+      const originalPostRes = await api.posts
+        .read({
+          include: 'authors',
+          slug: originalPostSlug
+        });
+      
+      const {
+        title,
+        published_at,
+        primary_author,
+        url
+      } = originalPostRes;
 
-    post.original_post = {
-      title,
-      published_at,
-      url,
-      primary_author
+      post.original_post = {
+        title,
+        url,
+        published_at,
+        primary_author,
+        locale_i18n
+      }
+    } catch(err) {
+      console.warn(err);
     }
   }
 
--- utils/shortcodes/sitemap-fetcher.js
+++ utils/shortcodes/sitemap-fetcher.js
@@ -1,63 +1,67 @@
 const fetch = require('node-fetch');
 const xml2js = require('xml2js');
 const { sourceApiUrl } = require('../ghost/api');
-const { siteURL } = require('../../config');
+const { siteURL, eleventyEnv } = require('../../config');
 const { escape } = require('lodash');
 
 const sitemapFetcherShortcode = async (page) => {
-  // will need some sort of map to handle all locales
-  const url =
-    page === 'index'
-      ? `${sourceApiUrl}/sitemap.xml`
-      : `${sourceApiUrl}/sitemap-${page}.xml`;
+  try {
+    const url =
+      page === 'index'
+        ? `${sourceApiUrl}/sitemap.xml`
+        : `${sourceApiUrl}/sitemap-${page}.xml`;
 
-  const ghostXml = await fetch(url)
-    .then((res) => res.text())
-    .then((res) => res)
-    .catch((err) => console.log(err));
+    const ghostXml = await fetch(url)
+      .then((res) => res.text())
+      .then((res) => res)
+      .catch((err) => console.log(err));
 
-  const parser = new xml2js.Parser();
-  const ghostXmlObj = await parser
-    .parseStringPromise(ghostXml)
-    .then((res) => res)
-    .catch((err) => console.log(err));
+    const parser = new xml2js.Parser();
+    const ghostXmlObj = await parser
+      .parseStringPromise(ghostXml)
+      .then((res) => res)
+      .catch((err) => console.log(err));
 
-  const target =
-    page === 'index'
-      ? ghostXmlObj.sitemapindex.sitemap
-      : ghostXmlObj.urlset.url;
+    const target =
+      page === 'index'
+        ? ghostXmlObj.sitemapindex.sitemap
+        : ghostXmlObj.urlset.url;
 
-  const urlSwapper = (url) => url.replace(sourceApiUrl, siteURL);
+    const urlSwapper = (url) => url.replace(sourceApiUrl, siteURL);
 
-  let xmlStr = target.reduce((acc, curr) => {
-    const wrapper = page === 'index' ? 'sitemap' : 'url';
+    let xmlStr = target.reduce((acc, curr) => {
+      const wrapper = page === 'index' ? 'sitemap' : 'url';
 
-    acc += `
-      <${wrapper}>
-      <loc>${urlSwapper(curr.loc[0])}</loc>
-      <lastmod>${curr.lastmod[0]}</lastmod>
-      ${
-        curr['image:image']
-          ? `
-        <image:image>
-          <image:loc>${escape(
-            urlSwapper(curr['image:image'][0]['image:loc'][0])
-          )}</image:loc>
-          <image:caption>${escape(
-            curr['image:image'][0]['image:caption'][0]
-          )}</image:caption>
-        </image:image>`
-          : ''
-      }
-    </${wrapper}>`;
+      acc += `
+        <${wrapper}>
+        <loc>${urlSwapper(curr.loc[0])}</loc>
+        <lastmod>${curr.lastmod[0]}</lastmod>
+        ${
+          curr['image:image']
+            ? `
+          <image:image>
+            <image:loc>${escape(
+              urlSwapper(curr['image:image'][0]['image:loc'][0])
+            )}</image:loc>
+            <image:caption>${escape(
+              curr['image:image'][0]['image:caption'][0]
+            )}</image:caption>
+          </image:image>`
+            : ''
+        }
+      </${wrapper}>`;
 
-    return acc;
-  }, '');
+      return acc;
+    }, '');
 
-  // xmlStr = xmlStr.replace(/\s+/g, '');
-
-  // To do: minify xml after build
-  return xmlStr;
+    return xmlStr;
+  } catch(err) {
+    // To do: remove this check once Dockerized Ghost is
+    // set up for testing
+    if (eleventyEnv !== 'ci') {
+      throw new Error(`Sitemap cannot be fetched`);
+    }
+  }
 };
 
 module.exports = sitemapFetcherShortcode;
