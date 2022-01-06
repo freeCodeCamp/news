638d4c5f639c77fa7d3f19d23f5724564f047ff6
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-01-06 20:10:41 +0900

    fix: move toggle-ad-layout script to monetization block, populate ads based on auth status

--- src/_includes/assets/js/toggle-ad-layout.js
+++ src/_includes/assets/js/toggle-ad-layout.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // May not be necessary
-  document.cookie = 'reader=1; SameSite=None; Secure';
-  const inner = document.querySelector('.inner');
-  const notAuthenticated = !document.cookie.split(';')
+// Global
+const notAuthenticated = !document.cookie.split(';')
     .some((item) => item.trim()
     .startsWith('jwt_access_token='));
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.cookie = 'reader=1; SameSite=None; Secure';
+  const inner = document.querySelector('.inner');
 
   if (notAuthenticated) {
     inner.classList.add('ad-layout');
