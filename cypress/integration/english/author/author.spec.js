486813f6fd56ff9f8b9cc44bd28161c1a8b9c6eb
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-02-09 00:47:25 +0900

    fix: bullet marks on author pages (#173)

--- cypress/integration/english/author/author.spec.js
+++ cypress/integration/english/author/author.spec.js
@@ -1,5 +1,8 @@
 const selectors = {
   authorName: "[data-test-label='author-name']",
+  authorLocation: "[data-test-label='author-location']",
+  authorPostCount: "[data-test-label='author-post-count']",
+  bullet: "[data-test-label='bullet']",
 };
 
 describe("Author page", () => {
@@ -9,5 +12,21 @@ describe("Author page", () => {
 
   it("should render", () => {
     cy.contains(selectors.authorName, "Quincy Larson");
+  });
+
+  it("should show the author's location and post count on larger screens", () => {
+    cy.get(selectors.authorLocation).should("be.visible");
+    cy.get(selectors.authorPostCount).should("be.visible");
+  });
+
+  it("should not show the author's location and post count screens < 500px", () => {
+    cy.viewport(499, 660);
+    cy.get(selectors.authorLocation).should("not.be.visible");
+    cy.get(selectors.authorPostCount).should("not.be.visible");
+  });
+
+  it("should not show any bullet marks on screens < 500px", () => {
+    cy.viewport(499, 660);
+    cy.get(selectors.bullet).should("not.be.visible");
   });
 });
