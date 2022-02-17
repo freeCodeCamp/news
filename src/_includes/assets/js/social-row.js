efc5a3e3e6e5082fea6e3c02381ce7fa3bbe6861
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-02-17 20:43:40 +0900

    feat: add tests, encode default tweet message

--- src/_includes/assets/js/social-row.js
+++ src/_includes/assets/js/social-row.js
@@ -2,16 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const title = "{{ post.title | urlencode }}".replace(/&#39;/g, "%27");
   const twitter = "{{ post.primary_author.twitter }}";
   const url = window.location;
-  const thanks =
-    `{% t 'social-row.default-tweet', { twitter: post.primary_author.twitter } %}` +
-    `%0A%0A${title}%0A%0A${url}`;
+  const escapedDefaultTweet =
+    "{% set defaultTweet %}{% t 'social-row.default-tweet', { twitter: post.primary_author.twitter } %}{% endset %}{{ defaultTweet | urlencode }}";
+  const thanks = `${escapedDefaultTweet}%0A%0A${title}%0A%0A${url}`;
   const button = document.getElementById("tweet-btn");
   const twitterIntentStr = twitter
     ? `https://twitter.com/intent/tweet?text=${thanks}`
     : `https://twitter.com/intent/tweet?text=${title}%0A%0A${url}`;
+  const windowOpenStr = `window.open(
+    '${twitterIntentStr}',
+    'share-twitter',
+    'width=550, height=235'
+  ); return false;`;
 
-  button.addEventListener("click", () => {
-    window.open(twitterIntentStr, "share-twitter", "width=550, height=235");
-    return false;
-  });
+  button.setAttribute("onclick", windowOpenStr);
 });
