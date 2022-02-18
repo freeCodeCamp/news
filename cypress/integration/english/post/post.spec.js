b452322c705397666a13ab550fe541ef2285545f
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-02-18 20:01:56 +0900

    fix: tweet button encoding (#187)

--- cypress/integration/english/post/post.spec.js
+++ cypress/integration/english/post/post.spec.js
@@ -1,3 +1,9 @@
+const selectors = {
+  comments: "[data-test-label='comments']",
+  socialRow: "[data-test-label='social-row']",
+  tweetButton: "[data-test-label='tweet-button']"
+};
+
 describe('Post', () => {
   before(() => {
     cy.visit('/announcing-rust-course-replit-web');
@@ -7,5 +13,26 @@ describe('Post', () => {
     cy.contains(
       "We're Building New Courses on Rust and Python + the Replit.web Framework"
     );
+  });
+
+  it('should not display a comments section', () => {
+    cy.get(selectors.comments).should('not.exist');
+  });
+
+  it('should display the social row', () => {
+    cy.get(selectors.socialRow).should('be.visible');
+  });
+
+  it('the tweet button should open a Twitter window with the correct message and dimensions', () => {
+    cy.get(selectors.tweetButton)
+      .invoke('attr', 'onclick')
+      .should('include', 'window.open')
+      .should(
+        'include',
+        'https://twitter.com/intent/tweet?text=Thank%20you%20%40ossia%20for%20writing%20this%20helpful%20article.%0A%0AWe%27re%20Building%20New%20Courses%20on%20Rust%20and%20Python%20%2B%20the%20Replit.web%20Framework%0A%0Ahttp://localhost:8080/news/announcing-rust-course-replit-web/'
+      )
+      .should('include', 'share-twitter')
+      .should('include', 'width=550, height=235')
+      .should('include', 'return false');
   });
 });
--- src/_includes/assets/js/social-row.js
+++ src/_includes/assets/js/social-row.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const title = '{{ post.title | escape }}'.replace(/&#39;/g, '%27');
+  const title = '{{ post.title | urlencode }}'.replace(/&#39;/g, '%27');
   const twitter = '{{ post.primary_author.twitter }}';
   const url = window.location;
-  const thanks =
-    `{% t 'social-row.default-tweet', { twitter: post.primary_author.twitter } %}` +
-    `%0A%0A${title}%0A%0A${url}`;
+  const escapedDefaultTweet =
+    "{% set defaultTweet %}{% t 'social-row.default-tweet', { twitter: post.primary_author.twitter } %}{% endset %}{{ defaultTweet | urlencode }}";
+  const thanks = `${escapedDefaultTweet}%0A%0A${title}%0A%0A${url}`;
   const button = document.getElementById('tweet-btn');
+  const twitterIntentStr = twitter
+    ? `https://twitter.com/intent/tweet?text=${thanks}`
+    : `https://twitter.com/intent/tweet?text=${title}%0A%0A${url}`;
   const windowOpenStr = `window.open(
-    '${
-      twitter
-        ? `https://twitter.com/intent/tweet?text=${thanks}`
-        : `https://twitter.com/intent/tweet?text=${title}%0A%0A${url}`
-    }',
+    '${twitterIntentStr}',
     'share-twitter',
     'width=550, height=235'
   ); return false;`;
