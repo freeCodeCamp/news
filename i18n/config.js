521a2583dac3e7cf0d1ed1a0a070807f7d4784fa
zaira <zairahira@gmail.com>
2022-04-13 13:37:28 +0500

    Translated to UR: meta-tags.json & translations.json

--- i18n/config.js
+++ i18n/config.js
@@ -24,12 +24,13 @@ i18next.use(Backend).init({
 
 (async () => {
   const url = `https://cdn.freecodecamp.org/universal/trending/${currentLocale_i18n}.yaml`;
-  const trendingYAML = await fetch(url)
+  /*const trendingYAML = await fetch(url)
     .then(res => {
       if (!res.ok) {
-        throw new Error(
-          `
-
+	      // throw new Error(
+            console.error(
+	 
+           `
 
           ----------------------------------------------------
           Error: The CDN is missing the trending YAML file.
@@ -37,13 +38,15 @@ i18next.use(Backend).init({
           Unable to fetch the ${currentLocale_i18n} footer: ${res.statusText}
 
 
-          `
+       `
         );
       }
       return res;
     })
     .then(res => res.text());
   const trendingJSON = yaml.load(trendingYAML);
+*/
+let trendingJSON
 
   i18next.addResources(currentLocale_i18nISOCode, 'trending', trendingJSON);
 })().catch(err => {
