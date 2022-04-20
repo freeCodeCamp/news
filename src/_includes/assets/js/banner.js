dbb9f1015b810140a04a0c97b44494f048d9283d
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2022-04-20 10:17:54 +0300

    feat: add banner variations

--- src/_includes/assets/js/banner.js
+++ src/_includes/assets/js/banner.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const bannerText = document.getElementById('banner-text');
 
-  // eslint-disable-next-line no-undef
-  if (notAuthenticated) {
-    bannerText.innerText = `{% t 'banner', {
+  const banner = `{% t 'banner', {
       '<0>': '<span>',
       '</0>': '</span>',
       interpolation: {
           escapeValue: false
       }
     } %}`;
+
+  // eslint-disable-next-line no-undef
+  if (notAuthenticated) {
+    bannerText.innerHTML = banner;
   } else {
     bannerText.innerText = `{% t 'embed-title' %}`;
   }
