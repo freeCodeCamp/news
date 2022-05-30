4935717cef8c85baee6f7f80c5dba91f0fdd2378
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-05-30 22:49:06 +0900

    feat: add more tests to landing.spec.js, refactor and reword existing tests

--- cypress/integration/espanol/landing/landing.spec.js
+++ cypress/integration/espanol/landing/landing.spec.js
@@ -7,7 +7,9 @@ const selectors = {
   translatorListItem: "[data-test-label='translator-list-item']",
   profileImage: "[data-test-label='profile-image']",
   profileLink: "[data-test-label='profile-link']",
-  postPublishedTime: "[data-test-label='post-published-time']"
+  postPublishedTime: "[data-test-label='post-published-time']",
+  authorName: 'Quincy Larson',
+  translatorName: 'Rafael D. Hernandez'
 };
 
 describe('Landing', () => {
@@ -26,16 +28,18 @@ describe('Landing', () => {
   });
 
   context('Original author / translator feature', () => {
-    it('the author list should contain two children', () => {
+    it('the author list should contain an author list item and a translator list item', () => {
       cy.get(selectors.postCard)
         .contains(selectors.translatedArticleTitle)
         .parentsUntil('article')
         .find(selectors.authorList)
-        .children()
-        .should('have.length', 2);
+        .then($el => {
+          cy.wrap($el).find(selectors.authorListItem);
+          cy.wrap($el).find(selectors.translatorListItem);
+        });
     });
 
-    it('the author should have profile image, profile link, and post published time elements', () => {
+    it('the author list item should have profile image, profile link, and post published time elements', () => {
       cy.get(selectors.postCard)
         .contains(selectors.translatedArticleTitle)
         .parentsUntil('article')
@@ -47,7 +51,58 @@ describe('Landing', () => {
         });
     });
 
-    it('the translator should have profile image, profile link, and post published time elements', () => {
+    it("the author list item's profile image should be wrapped in an anchor that points to the author's page", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.authorListItem)
+        .find(selectors.profileImage)
+        .parent()
+        .then($el => {
+          expect($el.attr('href')).to.deep.equal(
+            'https://www.freecodecamp.org/news/author/quincylarson/'
+          );
+        });
+    });
+
+    it("the author list item's profile link should contain the author's name and the locale of the original article", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.authorListItem)
+        .find(selectors.profileLink)
+        .contains(`${selectors.authorName} (inglés)`);
+    });
+
+    it("the author list item's profile link should be a full URL that points to the original author's page", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.authorListItem)
+        .find(selectors.profileLink)
+        .then($el => {
+          expect($el.attr('href')).to.deep.equal(
+            'https://www.freecodecamp.org/news/author/quincylarson/'
+          );
+        });
+    });
+
+    it("the author list item's post published time should convert to the expected UTC string", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.authorListItem)
+        .find(selectors.postPublishedTime)
+        .then($el => {
+          const publishedTimeUTC = new Date($el.attr('datetime')).toUTCString();
+
+          expect(publishedTimeUTC).to.deep.equal(
+            'Wed, 09 Dec 2020 16:20:00 GMT'
+          );
+        });
+    });
+
+    it('the translator list item should have profile image, profile link, and post published time elements', () => {
       cy.get(selectors.postCard)
         .contains(selectors.translatedArticleTitle)
         .parentsUntil('article')
@@ -56,6 +111,57 @@ describe('Landing', () => {
           cy.wrap($el).find(selectors.profileImage);
           cy.wrap($el).find(selectors.profileLink);
           cy.wrap($el).find(selectors.postPublishedTime);
+        });
+    });
+
+    it("the translator list item's profile image should be wrapped in an anchor that points to a relative URL of the translator's page on the current news instance", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.translatorListItem)
+        .find(selectors.profileImage)
+        .parent()
+        .then($el => {
+          expect($el.attr('href')).to.deep.equal(
+            '/espanol/news/author/rafael/'
+          );
+        });
+    });
+
+    it("the translator list item's profile link should contain the author's name and the locale of the original article", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.translatorListItem)
+        .find(selectors.profileLink)
+        .contains(selectors.translatorName);
+    });
+
+    it("the translator list item's profile link should be a relative URL of the translator's page on the current News instance", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.translatorListItem)
+        .find(selectors.profileLink)
+        .then($el => {
+          expect($el.attr('href')).to.deep.equal(
+            '/espanol/news/author/rafael/'
+          );
+        });
+    });
+
+    it("the translator list item's post published time should convert to the expected UTC string", () => {
+      cy.get(selectors.postCard)
+        .contains(selectors.translatedArticleTitle)
+        .parentsUntil('article')
+        .find(selectors.translatorListItem)
+        .find(selectors.postPublishedTime)
+        .then($el => {
+          const publishedTimeUTC = new Date($el.attr('datetime')).toUTCString();
+
+          expect(publishedTimeUTC).to.deep.equal(
+            'Sun, 16 Jan 2022 22:46:14 GMT'
+          );
         });
     });
   });
