530a19aca83af594a6a62f21259d10771fbd48cb
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2022-08-24 16:54:09 +0900

    feat: add test for ad-text i18n key, add they key and its value to all translation.json files except Chinese

--- utils/ghost/modify-ghost-html.js
+++ utils/ghost/modify-ghost-html.js
@@ -13,7 +13,9 @@ const generateAdHTML = type => {
   return `<div class="ad-container ${
     type === 'banner' ? 'banner' : ''
   }" data-test-label="ad-container">
-    <span class="ad-text" data-test-label="ad-text">ADVERTISEMENT</span>
+    <span class="ad-text" data-test-label="ad-text">${translate(
+      'ad-text'
+    ).toUpperCase()}</span>
     <ins
         class="adsbygoogle"
         data-ad-client="${googleAdsenseDataAdClient}"
