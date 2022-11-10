e76809b95335b0468cb54c63c73116b2f1a74852
Mrugesh Mohapatra <hi@mrugesh.dev>
2022-11-10 21:10:20 +0530

    fix: update cypress tests

--- cypress/e2e/chinese/amp/ads.cy.js
+++ cypress/e2e/chinese/amp/ads.cy.js
@@ -5,6 +5,8 @@ const selectors = {
 
 describe('Ads', () => {
   before(() => {
+    // Update baseUrl to include current language
+    Cypress.config('baseUrl', 'http://localhost:8080/chinese/news');
     cy.visit('/javascript-array-length');
   });
 
