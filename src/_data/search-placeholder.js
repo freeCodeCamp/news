dd45ce4f55a2557901ab365e5d1175d5b0f9bfca
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2022-11-14 18:59:56 +0900

    fix: simplify search-placeholder global data file

--- src/_data/search-placeholder.js
+++ src/_data/search-placeholder.js
@@ -5,20 +5,16 @@ const {
   currentLocale_i18nISOCode,
   eleventyEnv
 } = require('../../config');
-const { readFileSync } = require('fs');
-const { join } = require('path');
 const algoliasearch = require('algoliasearch/lite');
 
 const roundDownToNearestHundred = num => Math.floor(num / 100) * 100;
-const convertToLocalizedString = (num, ISOCode) => num.toLocaleString(ISOCode);
+const convertToLocalizedString = (num, ISOCode) => num.toLocaleString(ISOCode); // Uses commas or decimals depending on the locale
 
 module.exports = async () => {
   let roundedTotalHits;
 
   if (eleventyEnv === 'ci') {
-    const mockHits = readFileSync(
-      join(__dirname, '../../cypress/fixtures/mock-search-hits.json')
-    );
+    const mockHits = require('../../cypress/fixtures/mock-search-hits.json');
 
     roundedTotalHits = roundDownToNearestHundred(mockHits.length);
   } else {
