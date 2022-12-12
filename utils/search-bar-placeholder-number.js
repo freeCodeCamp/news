b8a6c379777ea17068942254a12f0a367fe3d429
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2022-12-12 22:14:29 +0900

    fix: improve modularization

--- utils/search-bar-placeholder-number.js
+++ utils/search-bar-placeholder-number.js
@@ -1,7 +1,48 @@
+const algoliasearch = require('algoliasearch/lite');
+const {
+  algoliaAppId,
+  algoliaAPIKey,
+  algoliaIndex,
+  eleventyEnv
+} = require('../config');
+
 const roundDownToNearestHundred = num => Math.floor(num / 100) * 100;
 const convertToLocalizedString = (num, ISOCode) => num.toLocaleString(ISOCode); // Use commas or decimals depending on the locale
 
+const getRoundedTotalEntries = async () => {
+  let totalEntries = 0;
+
+  try {
+    if (eleventyEnv === 'ci') {
+      const mockHits = require('../cypress/fixtures/mock-search-hits.json');
+
+      totalEntries = mockHits.length;
+    } else {
+      const client = algoliasearch(algoliaAppId, algoliaAPIKey);
+      const index = client.initIndex(algoliaIndex);
+      const res = await index.search('');
+
+      totalEntries = res?.nbHits;
+    }
+  } catch (err) {
+    console.warn(`
+      ----------------------------------------------------------
+      Warning: Could not get the total number of Algolia entries
+      ----------------------------------------------------------
+      Make sure that Algolia keys and index are set up correctly,
+      or that the mock search hits are available if running in CI
+      mode.
+
+      Falling back to the default search placeholder text.
+      ----------------------------------------------------------
+  `);
+  }
+
+  return roundDownToNearestHundred(totalEntries);
+};
+
 module.exports = {
   roundDownToNearestHundred,
-  convertToLocalizedString
+  convertToLocalizedString,
+  getRoundedTotalEntries
 };
