bd87b348a9e09bfedf3651d558160d159d67f6df
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-06-13 16:58:27 +0900

    fix: remove json-merge-patch, simplify serve.json generation, add common /slug/amp to /slug redirect

--- config/i18n/generate-serve-config.js
+++ config/i18n/generate-serve-config.js
@@ -1,22 +1,26 @@
 const path = require('path');
 const { writeFileSync, mkdirSync } = require('fs');
-const jsonmergepatch = require('json-merge-patch');
 const { locales } = require('../index.js');
 
-let source = require('../serve.json');
-
+const source = require('../serve.json');
 locales.push('dothraki');
-for (let language of locales) {
-  const filePath = path.join(__dirname, `/locales/${language}/serve.json`);
-  const patch = require(filePath);
 
-  source = jsonmergepatch.apply(source, patch);
+for (let language of locales) {
+  const sourceClone = { ...source };
+  const filePath = path.join(__dirname, `/locales/${language}/redirects.json`);
+  const redirectsArray = require(filePath);
+
+  if (redirectsArray.length)
+    sourceClone.redirects = [...sourceClone.redirects, ...redirectsArray];
+
   mkdirSync(path.join(__dirname, `../../docker/languages/${language}`), {
     recursive: true
   });
+
   writeFileSync(
     path.join(__dirname, `../../docker/languages/${language}/serve.json`),
-    JSON.stringify(source, null, 2)
+    JSON.stringify(sourceClone, null, 2)
   );
+
   console.log(`Wrote ${language}/serve.json`);
 }
