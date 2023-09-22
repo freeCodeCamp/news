7ff58058ce155c3fd233a5e8f8c3bd6c9fe0e086
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-09-22 19:05:25 +0900

    revert: add testIsolation

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,7 @@ const { postsPerPage } = require('./config');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080/news',
-    retries: 4,
-    testIsolation: false
+    retries: 4
   },
   env: {
     postsPerPage
