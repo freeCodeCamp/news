d1bfcb21a6a53ede9d176d0f909b1d327101cb27
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-09-22 19:02:10 +0900

    fix: set testIsolation to false

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,8 @@ const { postsPerPage } = require('./config');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080/news',
-    retries: 4
+    retries: 4,
+    testIsolation: false
   },
   env: {
     postsPerPage
