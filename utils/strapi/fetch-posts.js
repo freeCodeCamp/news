71c269add62588b92fbdbbaf4822a6c2b86822d8
Niraj Nandish <nirajnandish@icloud.com>
2023-10-30 00:10:28 +0400

    feat: fetch all posts using paginated queries

--- utils/strapi/fetch-posts.js
+++ utils/strapi/fetch-posts.js
@@ -3,32 +3,63 @@ const { strapiApiUrl, strapiAccessToken } = require('../../config');
 const stripDomain = require('../strip-domain');
 const qs = require('qs');
 
-const fetchPosts = async contentType => {
+const wait = seconds => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(seconds);
+    }, seconds * 1000);
+  });
+};
+
+const fetchPosts = async () => {
+  let currPage = 1;
+  let lastPage = 5;
+  let data = [];
   const options = {
     publicationState: 'live',
     populate: ['tags', 'author', 'feature_image', 'author.profile_image'],
     sort: ['publishedAt:desc'],
     pagination: {
-      limit: 10
+      pageSize: 100
     }
   };
 
-  const res = await fetch(
-    `${strapiApiUrl}/posts?${qs.stringify(options, {
-      encodeValuesOnly: true
-    })}`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${strapiAccessToken}`
+  while (currPage && currPage <= lastPage) {
+    const strapiRes = await fetch(
+      `${strapiApiUrl}/posts?${qs.stringify(
+        {
+          ...options,
+          pagination: {
+            ...options.pagination,
+            page: currPage
+          }
+        },
+        {
+          encodeValuesOnly: true
+        }
+      )}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${strapiAccessToken}`
+        }
       }
-    }
-  ).catch(err => {
-    console.error(err);
-  });
+    ).catch(err => {
+      console.error(err);
+    });
 
-  const data = await res.json();
-  return data.data; // data.meta needs to be used for pagination
+    const strapiData = await strapiRes.json();
+    lastPage = strapiData.meta.pagination.pageCount;
+    if (strapiData.data.length > 0)
+      console.log(`Fetched posts page ${currPage} of ${lastPage}...`);
+    currPage += 1;
+
+    data = data.concat(strapiData.data);
+
+    await wait(0.2);
+  }
+
+  return data;
 };
 
 module.exports = fetchPosts;
