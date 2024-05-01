cee179293fa8ecc82a7488a0e6cbfd16afaea2d7
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-05-01 15:22:24 +0900

    feat: add test for tag pages

--- cypress/e2e/english/tag/tag.cy.js
+++ cypress/e2e/english/tag/tag.cy.js
@@ -8,6 +8,23 @@ const selectors = {
 };
 
 describe('Tag pages', () => {
+  context('General tests', () => {
+    // Tests here should apply to all tag pages, regardless of the source
+    beforeEach(() => {
+      cy.visit('/tag/gamedev/');
+    });
+
+    // Note: This test can be removed once we migrate everything to Hashnode.
+    it('pages for tags with the same slug on both Ghost and Hashnode should show all posts', () => {
+      // Hashnode sourced post
+      cy.contains(
+        'Ben Awad is a GameDev Who Sleeps 9 Hours EVERY NIGHT to be Productive [Podcast #121]'
+      );
+      // Ghost sourced post
+      cy.contains('Learn to Code RPG – Full Soundtrack + How I Made It');
+    });
+  });
+
   context('Ghost sourced tags', () => {
     beforeEach(() => {
       cy.visit('/tag/javascript/');
