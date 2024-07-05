bce1b7e7d9864d4df5c2010b5eff4f2d159f64db
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-07-05 21:31:57 +0900

    fix: logging while running in dev or ci mode

--- utils/ping-editorial-team.js
+++ utils/ping-editorial-team.js
@@ -21,12 +21,13 @@ WARNING: Duplicate Posts Found
 -----------------------------------------------
 ${msg}
 `);
+  console.error(msg);
   errorLogger({ type: 'duplicate-posts', name: msg });
 
-  try {
-    // Prevent sending messages while in dev or CI environments
-    if (eleventyEnv === 'dev' || eleventyEnv === 'ci') return;
+  // Prevent sending messages while in dev or CI environments
+  if (eleventyEnv === 'dev' || eleventyEnv === 'ci') return;
 
+  try {
     const chatWebhookURL = `https://chat.googleapis.com/v1/spaces/AAAAHMCb1fg/messages?key=${chatWebhookKey}&token=${chatWebhookToken}`;
     const res = await fetch(chatWebhookURL, {
       method: 'POST',
