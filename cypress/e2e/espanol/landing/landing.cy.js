293b89c6df60139713344a309bbdcebf7704167d
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-07-31 18:28:04 +0900

    fix: remove unused variables

--- cypress/e2e/espanol/landing/landing.cy.js
+++ cypress/e2e/espanol/landing/landing.cy.js
@@ -1,17 +1,3 @@
-const selectors = {
-  postCard: "[data-test-label='post-card']",
-  translatedArticleTitle:
-    'El Desafío #100DaysOfCode, su historia y por qué debes hacerlo para el 2021',
-  authorList: "[data-test-label='author-list']",
-  authorListItem: "[data-test-label='author-list-item']",
-  translatorListItem: "[data-test-label='translator-list-item']",
-  profileImage: "[data-test-label='profile-image']",
-  profileLink: "[data-test-label='profile-link']",
-  postPublishedTime: "[data-test-label='post-published-time']",
-  authorName: 'Quincy Larson',
-  translatorName: 'Rafael D. Hernandez'
-};
-
 describe('Landing', () => {
   before(() => {
     // Update baseUrl to include current language
