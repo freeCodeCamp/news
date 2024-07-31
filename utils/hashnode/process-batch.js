82404ec30d332568084b0bf39d98cefbbefcfde7
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-07-31 15:48:06 +0900

    fix: improve fallback for translated articles when there is an error

--- utils/hashnode/process-batch.js
+++ utils/hashnode/process-batch.js
@@ -15,87 +15,90 @@ const processBatch = async ({
   );
 
   await Promise.all(
-    batch.map(async oldObj => {
-      const newObj = {};
-
-      newObj.id = oldObj.id;
-      newObj.slug = oldObj.slug;
-      newObj.path = `/${oldObj.slug}/`;
-      newObj.title = oldObj.title;
+    batch.map(async obj => {
+      obj.path = `/${obj.slug}/`;
       // Set the source of the publication and whether it's a page or post for tracking and later processing
-      newObj.source = 'Hashnode';
+      obj.source = 'Hashnode';
       const singularContentType = contentType.slice(0, -1);
-      newObj.contentType = singularContentType;
+      obj.contentType = singularContentType;
 
-      newObj.html = await modifyHTMLContent({
-        postContent: oldObj.content.html,
-        postTitle: newObj.title,
-        source: newObj.source
+      obj.html = await modifyHTMLContent({
+        postContent: obj.content.html,
+        postTitle: obj.title,
+        source: obj.source
       });
 
       // Hashnode pages don't have a brief that we can cast as an excerpt or original_excerpt,
       // so we strip the HTML tags from the body text to generate a brief for SEO and structured data.
-      if (!Object.hasOwn(oldObj, 'brief')) {
-        const sanitizedBodyText = stripHTMLTags(newObj.html);
-        oldObj.brief = shortenExcerpt(sanitizedBodyText);
+      if (!Object.hasOwn(obj, 'brief')) {
+        const sanitizedBodyText = stripHTMLTags(obj.html);
+        obj.brief = shortenExcerpt(sanitizedBodyText);
       }
 
       // Note: Longer posts include an ellipsis. We can decide how to
       // handle this in the future.
-      newObj.original_excerpt = oldObj.brief;
-      newObj.excerpt = shortenExcerpt(oldObj.brief);
+      obj.original_excerpt = obj.brief;
+      obj.excerpt = shortenExcerpt(obj.brief);
+      delete obj.brief;
 
       if (contentType === 'posts') {
         // Set a default feature image for posts if one doesn't exist
         // Note: We're not handling pages from Hashnode, so there's no
         // need to handle cases where we may not want to have a cover image
         // for a particular page.
-        newObj.feature_image = oldObj?.coverImage?.url
-          ? oldObj.coverImage.url
+        obj.feature_image = obj?.coverImage?.url
+          ? obj.coverImage.url
           : 'https://cdn.freecodecamp.org/platform/universal/fcc_meta_1920X1080-indigo.png';
+        delete obj.coverImage;
 
-        newObj.image_dimensions = {};
-        newObj.image_dimensions.feature_image = await getImageDimensions(
-          newObj.feature_image,
-          `Hashnode ${singularContentType} feature image: ${newObj.title}`
-        );
+        obj.image_dimensions = {
+          feature_image: await getImageDimensions(
+            obj.feature_image,
+            `Hashnode ${singularContentType} feature image: ${obj.title}`
+          )
+        };
 
-        const newObjAuthor = {};
-        newObjAuthor.id = oldObj.author.id;
-        newObjAuthor.name = oldObj.author.name;
-        newObjAuthor.slug = oldObj.author.username;
-        newObjAuthor.bio = oldObj.author.bio.text;
-        newObjAuthor.location = oldObj.author.location;
-        newObjAuthor.website = oldObj.author.socialMediaLinks.website;
-        // Note: Mutate Twitter and Facebook links so they're just the username like
-        // on Ghost for now.
-        // TODO: Simplify social media links and how they're used throughout the build
-        // in the future.
-        newObjAuthor.twitter = oldObj.author.socialMediaLinks.twitter
-          ? oldObj.author.socialMediaLinks.twitter.replace(
-              'https://twitter.com/',
-              '@'
-            )
-          : null;
-        newObjAuthor.facebook = oldObj.author.socialMediaLinks.facebook
-          ? oldObj.author.socialMediaLinks.facebook.replace(
-              'https://www.facebook.com/',
-              ''
-            )
-          : null;
-        newObjAuthor.path = `/author/${oldObj.author.username}/`;
-        if (oldObj.author.profilePicture) {
-          newObjAuthor.profile_image = oldObj.author.profilePicture;
-          newObjAuthor.image_dimensions = {};
-          newObjAuthor.image_dimensions.profile_image =
-            await getImageDimensions(
-              newObjAuthor.profile_image,
-              `Hashnode author profile image: ${newObjAuthor.name}`
-            );
+        if (obj?.author) {
+          obj.primary_author = {
+            id: obj.author.id,
+            name: obj.author.name,
+            slug: obj.author.username,
+            path: `/author/${obj.author.username}/`,
+            bio: obj.author.bio.text,
+            location: obj.author.location,
+            website: obj.author.socialMediaLinks.website,
+            // Note: Mutate Twitter and Facebook links so they're just the username like
+            // on Ghost for now.
+            // TODO: Simplify social media links and how they're used throughout the build
+            // in the future.
+            twitter: obj.author.socialMediaLinks.twitter
+              ? obj.author.socialMediaLinks.twitter.replace(
+                  'https://twitter.com/',
+                  '@'
+                )
+              : null,
+            facebook: obj.author.socialMediaLinks.facebook
+              ? obj.author.socialMediaLinks.facebook.replace(
+                  'https://www.facebook.com/',
+                  ''
+                )
+              : null
+          };
+
+          if (obj.author.profilePicture) {
+            obj.primary_author.profile_image = obj.author.profilePicture;
+            obj.primary_author.image_dimensions = {
+              profile_image: await getImageDimensions(
+                obj.primary_author.profile_image,
+                `Hashnode author profile image: ${obj.name}`
+              )
+            };
+          }
+
+          delete obj.author;
         }
-        newObj.primary_author = newObjAuthor;
 
-        newObj.tags = oldObj.tags.map(tag => {
+        obj.tags = obj.tags.map(tag => {
           tag.path = `/tag/${tag.slug}/`;
           // TODO: Setting all tags as public for now. Have to decide how we'll
           // handle private tags.
@@ -103,20 +106,24 @@ const processBatch = async ({
           return tag;
         });
 
-        newObj.published_at = oldObj.publishedAt;
-        newObj.updated_at = oldObj.updatedAt;
-        newObj.reading_time = oldObj.readTimeInMinutes;
+        obj.published_at = obj.publishedAt;
+        obj.updated_at = obj.updatedAt;
+        obj.reading_time = obj.readTimeInMinutes;
+        delete obj.publishedAt;
+        delete obj.updatedAt;
+        delete obj.readTimeInMinutes;
 
         // Note: We only use the SEO description for the original post / translator
         // feature, and don't use it for structured data or SEO for built pages, so
-        // it doesn't have to be added to the newObj mimicking a Ghost post.
-        if (oldObj?.seo?.description) {
+        // it doesn't have to included in the final returned object.
+        if (obj?.seo?.description) {
           const originalPostData = await originalPostHandler(
-            oldObj.seo.description
+            obj.seo.description
           );
 
-          newObj.original_post = originalPostData;
-          newObj.html = originalPostData?.introHTML + newObj.html;
+          obj.original_post = originalPostData;
+          obj.html = originalPostData?.introHTML + obj.html;
+          delete obj.seo;
         }
       }
 
@@ -124,6 +131,7 @@ const processBatch = async ({
     })
   );
 
+  console.log(batch);
   return batch;
 };
 
