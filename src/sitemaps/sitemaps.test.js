86701b47b25da6d034f96c95b5da0bb35575ebf6
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-08-14 22:09:29 +0900

    feat: update sitemap tests

--- src/sitemaps/sitemaps.test.js
+++ src/sitemaps/sitemaps.test.js
@@ -1,5 +1,5 @@
 const libxmljs = require('libxmljs');
-const fs = require('fs');
+const { readFileSync } = require('graceful-fs');
 const { join } = require('path');
 
 const sitemapFilenames = [
@@ -16,13 +16,10 @@ describe('Sitemap tests:', () => {
     sitemapFilenames.forEach(sitemapFilename => {
       test(`${sitemapFilename} is valid`, () => {
         try {
-          const sitemap = fs.readFileSync(
-            join(distPath, sitemapFilename),
-            'utf8'
-          );
+          const sitemap = readFileSync(join(distPath, sitemapFilename), 'utf8');
           const schemaFilename =
             sitemapFilename === 'sitemap.xml' ? 'siteindex.xsd' : 'sitemap.xsd';
-          const schema = fs.readFileSync(
+          const schema = readFileSync(
             join(__dirname, `./schemas/${schemaFilename}`),
             'utf8'
           );
@@ -41,6 +38,40 @@ describe('Sitemap tests:', () => {
           // Throw error again to fail test if a sitemap cannot be parsed correctly
           throw new Error(err);
         }
+      });
+    });
+  });
+
+  describe('Posts sitemap', () => {
+    describe('Last modified date', () => {
+      test('All posts should have a valid last modified date', () => {
+        const postsSitemap = readFileSync(
+          join(distPath, 'sitemap-posts.xml'),
+          'utf8'
+        );
+        const postsSitemapDoc = libxmljs.parseXml(postsSitemap);
+        const postURLNodes = postsSitemapDoc
+          .root()
+          .childNodes()
+          .filter(node => {
+            return node.name() === 'url';
+          });
+        const lastmodDates = postURLNodes
+          .map(node => {
+            return node
+              .childNodes()
+              .filter(node => node.name() === 'lastmod')
+              .map(node => node.text());
+          })
+          .flat();
+
+        // Ensure all posts have a last modified date
+        expect(lastmodDates).toHaveLength(postURLNodes.length);
+
+        // Ensure all last modified dates are valid dates
+        lastmodDates.forEach(date => {
+          expect(new Date(date)).toBeInstanceOf(Date);
+        });
       });
     });
   });
