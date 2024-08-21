72bd4cbddd6ab8709ccff2f95d25c63a49a15e6c
Anna <a.rcottrill521@gmail.com>
2024-08-21 04:06:01 -0400

    fix(ui): update banner text when auth and donor (#957)

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:cypress/recommended', 'prettier'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
+    isDonor: 'writable',
+    isAuthenticated: 'writable'
   },
   parserOptions: {
     ecmaVersion: 2020
