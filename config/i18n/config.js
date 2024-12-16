420d5cd6972a4b4a128d8a540b475eccce65c8a8
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2024-12-16 19:31:53 +0900

    feat: download trending footer links json before builds (#1083)

--- config/i18n/config.js
+++ config/i18n/config.js
@@ -1,7 +1,5 @@
 const i18next = require('i18next');
 const Backend = require('i18next-fs-backend');
-const fetch = require('node-fetch');
-const yaml = require('js-yaml');
 const { readdirSync, lstatSync } = require('fs');
 const { join } = require('path');
 const {
@@ -18,40 +16,11 @@ i18next.use(Backend).init({
     const isDirectory = lstatSync(joinedPath).isDirectory();
     return isDirectory;
   }),
-  ns: ['translations', 'meta-tags', 'links'],
+  ns: ['translations', 'meta-tags', 'links', 'trending'],
   defaultNS: 'translations',
   backend: {
     loadPath: join(__dirname, `./locales/${currentLocale_i18n}/{{ ns }}.json`)
   }
-});
-
-(async () => {
-  const url = `https://cdn.freecodecamp.org/universal/trending/${currentLocale_i18n}.yaml`;
-  const trendingYAML = await fetch(url)
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(
-          `
-
-
-          ----------------------------------------------------
-          Error: The CDN is missing the trending YAML file.
-          ----------------------------------------------------
-          Unable to fetch the ${currentLocale_i18n} footer: ${res.statusText}
-
-
-          `
-        );
-      }
-      return res;
-    })
-    .then(res => res.text());
-  const trendingJSON = yaml.load(trendingYAML);
-
-  i18next.addResources(currentLocale_i18nISOCode, 'trending', trendingJSON);
-})().catch(err => {
-  console.log(err); // Log js-yaml or fetch errors
-  process.exit(1); // Forcefully exit
 });
 
 module.exports = i18next;
---
+++ tools/download-trending.js
@@ -0,0 +1,82 @@
+const { readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+const fetch = require('node-fetch');
+const yaml = require('js-yaml');
+
+const { currentLocale_i18n } = require('../config');
+const trendingSchemaValidator = require('./schemas/trending-schema');
+
+const download = async clientLocale => {
+  const trendingURL = `https://cdn.freecodecamp.org/universal/trending/${clientLocale}.yaml`;
+  const trendingLocation = path.resolve(
+    __dirname,
+    `../config/i18n/locales/${clientLocale}/trending.json`
+  );
+
+  const loadLocalTrendingJSON = () => {
+    const localTrendingJSON = readFileSync(trendingLocation, 'utf8');
+
+    if (!localTrendingJSON) {
+      throw new Error(
+        `
+        ----------------------------------------------------
+        Error: ${trendingLocation} is missing.
+        ----------------------------------------------------
+        `
+      );
+    }
+
+    return localTrendingJSON;
+  };
+
+  const loadTrendingJSON = async () => {
+    try {
+      const res = await fetch(trendingURL);
+
+      if (!res.ok) {
+        throw new Error(
+          `
+          ----------------------------------------------------
+          Error: The CDN is missing the trending YAML file.
+          ----------------------------------------------------
+          Unable to fetch the ${clientLocale} footer: ${res.statusText}
+          `
+        );
+      }
+
+      const data = await res.text();
+      const trendingJSON = JSON.stringify(yaml.load(data));
+
+      return trendingJSON;
+    } catch (err) {
+      if (process.env.FREECODECAMP_NODE_ENV === 'production') {
+        throw new Error(err.message);
+      }
+
+      return loadLocalTrendingJSON();
+    }
+  };
+
+  const trendingJSON = await loadTrendingJSON();
+
+  writeFileSync(trendingLocation, trendingJSON);
+
+  const trendingObject = JSON.parse(trendingJSON);
+  const validationError = trendingSchemaValidator(trendingObject).error || null;
+
+  if (validationError) {
+    throw new Error(
+      `
+    ----------------------------------------------------
+    Error: The trending JSON is invalid.
+    ----------------------------------------------------
+    Unable to validate the ${clientLocale} trending JSON schema: ${validationError.message}
+    `
+    );
+  }
+};
+
+if (!currentLocale_i18n)
+  throw Error('currentLocale_i18n must be set to a valid locale');
+
+download(currentLocale_i18n);
