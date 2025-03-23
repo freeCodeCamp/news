9fb9f9e79421bc8da07349340dd79693fb7a6014
scissorsneedfoodtoo <2051070+scissorsneedfoodtoo@users.noreply.github.com>
2025-03-23 17:43:48 +0900

    fix: remove probe-image, roll our own solution

--- utils/get-image-dimensions.js
+++ utils/get-image-dimensions.js
@@ -1,14 +1,43 @@
-const probe = require('probe-image-size');
+const fetch = require('node-fetch');
+const { imageSize } = require('image-size');
 const errorLogger = require('./error-logger');
 const { getCache, setCache } = require('./cache');
 const defaultDimensions = { width: 600, height: 400 };
 
+const probeImage = async url => {
+  const response = await fetch(url);
+  if (!response.ok)
+    throw new Error(`Failed to fetch image: ${response.statusText}`);
+
+  const chunks = [];
+  const reader = response.body;
+
+  for await (const chunk of reader) {
+    chunks.push(chunk);
+
+    try {
+      // Try to determine image size with available data
+      const dimensions = imageSize(Buffer.concat(chunks));
+      if (dimensions.width && dimensions.height) {
+        response.body.destroy(); // Stop data stream
+        return dimensions;
+      }
+    } catch (err) {
+      // Keep reading if more data is needed
+    }
+  }
+
+  throw new Error('Could not determine image size');
+};
+
 const getImageDimensions = async (url, description) => {
   try {
+    if (url.startsWith('data:'))
+      throw new Error('Data URI images are not supported');
     let imageDimensions = getCache(url);
     if (imageDimensions) return imageDimensions;
 
-    const res = await probe(url);
+    const res = await probeImage(url);
     imageDimensions = {
       width: res?.width ? res?.width : defaultDimensions.width,
       height: res?.height ? res?.height : defaultDimensions.height
@@ -17,7 +46,7 @@ const getImageDimensions = async (url, description) => {
 
     return imageDimensions;
   } catch (err) {
-    if (err.statusCode) errorLogger({ type: 'image', name: description }); // Only write HTTP status code errors to log
+    errorLogger({ type: 'image', name: description });
 
     return defaultDimensions;
   }
