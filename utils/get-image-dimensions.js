80e4a2883473c703d67ddc147bfc48d6c0d3fba8
scissorsneedfoodtoo <2051070+scissorsneedfoodtoo@users.noreply.github.com>
2025-03-24 17:27:27 +0900

    fix: reduce batch size, clean up get image dimensions function

--- utils/get-image-dimensions.js
+++ utils/get-image-dimensions.js
@@ -1,9 +1,10 @@
 const probe = require('probe-image-size');
 const errorLogger = require('./error-logger');
 const { getCache, setCache } = require('./cache');
-const defaultDimensions = { width: 600, height: 400 };
 
 const getImageDimensions = async (url, description) => {
+  let imageDimensions = { width: 600, height: 400 };
+
   try {
     if (url.startsWith('data:')) {
       console.warn(`
@@ -12,71 +13,27 @@ const getImageDimensions = async (url, description) => {
         ---------------------------------------------------------------
         `);
 
-      // throw new Error('Data URL');
-      return defaultDimensions;
+      throw new Error('Data URL');
     }
-    let imageDimensions = getCache(url);
-    if (imageDimensions) return imageDimensions;
+    const cachedDimensions = getCache(url);
+    if (cachedDimensions) imageDimensions = cachedDimensions;
 
-    const res = await probe(url, {
-      open_timeout: 5000,
-      response_timeout: 3000,
-      read_timeout: 3000
-      // rejectUnauthorized: true
-    });
-    // const res = await probe(url);
+    const fetchedImageDimensions = await probe(url);
     imageDimensions = {
-      width: res?.width ? res?.width : defaultDimensions.width,
-      height: res?.height ? res?.height : defaultDimensions.height
+      width: fetchedImageDimensions?.width
+        ? fetchedImageDimensions?.width
+        : imageDimensions.width,
+      height: fetchedImageDimensions?.height
+        ? fetchedImageDimensions?.height
+        : imageDimensions.height
     };
-    setCache(url, imageDimensions);
 
-    return imageDimensions;
+    setCache(url, imageDimensions);
   } catch (err) {
     errorLogger({ type: 'image', name: description });
-
-    return defaultDimensions;
+  } finally {
+    return imageDimensions;
   }
 };
-
-// const getImageDimensions = async (url, description) => {
-//   // try {
-//   //   if (url.startsWith('data:')) {
-//   //     console.warn(`
-//   //       ---------------------------------------------------------------
-//   //       Warning: Skipping data URL for image dimensions in ${description.substring(0, 350)}...
-//   //       ---------------------------------------------------------------
-//   //       `);
-
-//   //     return defaultDimensions;
-//   //   }
-//   //   const res = await probe(url);
-
-//   //   const imageDimensions = {
-//   //     width: res?.width ? res?.width : defaultDimensions.width,
-//   //     height: res?.height ? res?.height : defaultDimensions.height
-//   //   };
-
-//   //   return imageDimensions;
-//   // } catch (err) {
-//   //   errorLogger({ type: 'image', name: description });
-
-//   //   return defaultDimensions;
-//   // }
-
-//   try {
-//     const res = await probe(url);
-//     const imageDimensions = {
-//       width: res?.width ? res?.width : defaultDimensions.width,
-//       height: res?.height ? res?.height : defaultDimensions.height
-//     };
-
-//     return imageDimensions;
-//   } catch (err) {
-//     errorLogger({ type: 'image', name: description });
-
-//     return defaultDimensions;
-//   }
-// };
 
 module.exports = getImageDimensions;
