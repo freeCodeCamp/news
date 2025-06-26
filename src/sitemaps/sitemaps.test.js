308685ca887e03cfe5559ef1038476f47f115cca
scissorsneedfoodtoo <2051070+scissorsneedfoodtoo@users.noreply.github.com>
2025-06-26 14:44:27 +0900

    fix: restore xml validation tests, add node-gyp package to get things working

--- src/sitemaps/sitemaps.test.js
+++ src/sitemaps/sitemaps.test.js
@@ -1,88 +1,80 @@
+import libxmljs from 'libxmljs';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const { parseXml } = libxmljs;
+
+const sitemapFilenames = [
+  'sitemap.xml',
+  'sitemap-pages.xml',
+  'sitemap-posts.xml',
+  'sitemap-tags.xml',
+  'sitemap-authors.xml'
+];
+const distPath = join(import.meta.dirname, '../../dist');
+
 describe('Sitemap tests:', () => {
-  test('Sitemap tests are skipped', () => {
-    // This is a placeholder test to indicate that the actual sitemap tests are skipped.
-    // We'll need to find another XML parser that works with newer versions of Node and ESM.
-    expect(true).toBeTruthy();
+  describe('Validate sitemaps against schemas', () => {
+    sitemapFilenames.forEach(sitemapFilename => {
+      test(`${sitemapFilename} is valid`, () => {
+        try {
+          const sitemap = readFileSync(join(distPath, sitemapFilename), 'utf8');
+          const schemaFilename =
+            sitemapFilename === 'sitemap.xml' ? 'siteindex.xsd' : 'sitemap.xsd';
+          const schema = readFileSync(
+            join(import.meta.dirname, `./schemas/${schemaFilename}`),
+            'utf8'
+          );
+          const sitemapDoc = parseXml(sitemap);
+          const schemaDoc = parseXml(schema);
+          const isValid = sitemapDoc.validate(schemaDoc);
+
+          if (!isValid) {
+            throw new Error(
+              `${sitemapFilename} is not valid: ${sitemapDoc.validationErrors}`
+            );
+          }
+
+          expect(isValid).toBeTruthy();
+        } catch (err) {
+          // Throw error again to fail test if a sitemap cannot be parsed correctly
+          throw new Error(err);
+        }
+      });
+    });
+  });
+
+  describe('Posts sitemap', () => {
+    describe('Last modified date', () => {
+      test('All posts should have a valid last modified date', () => {
+        const postsSitemap = readFileSync(
+          join(distPath, 'sitemap-posts.xml'),
+          'utf8'
+        );
+        const postsSitemapDoc = parseXml(postsSitemap);
+        const postURLNodes = postsSitemapDoc
+          .root()
+          .childNodes()
+          .filter(node => {
+            return node.name() === 'url';
+          });
+        const lastmodDates = postURLNodes
+          .map(node => {
+            return node
+              .childNodes()
+              .filter(node => node.name() === 'lastmod')
+              .map(node => node.text());
+          })
+          .flat();
+
+        // Ensure all posts have a last modified date
+        expect(lastmodDates).toHaveLength(postURLNodes.length);
+
+        // Ensure all last modified dates are valid dates
+        lastmodDates.forEach(date => {
+          expect(new Date(date)).toBeInstanceOf(Date);
+        });
+      });
+    });
   });
 });
-
-// import libxmljs from 'libxmljs';
-// import { readFileSync } from 'fs';
-// import { join } from 'path';
-
-// const { parseXml } = libxmljs;
-
-// const sitemapFilenames = [
-//   'sitemap.xml',
-//   'sitemap-pages.xml',
-//   'sitemap-posts.xml',
-//   'sitemap-tags.xml',
-//   'sitemap-authors.xml'
-// ];
-// const distPath = join(import.meta.dirname, '../../dist');
-
-// describe('Sitemap tests:', () => {
-//   describe('Validate sitemaps against schemas', () => {
-//     sitemapFilenames.forEach(sitemapFilename => {
-//       test(`${sitemapFilename} is valid`, () => {
-//         try {
-//           const sitemap = readFileSync(join(distPath, sitemapFilename), 'utf8');
-//           const schemaFilename =
-//             sitemapFilename === 'sitemap.xml' ? 'siteindex.xsd' : 'sitemap.xsd';
-//           const schema = readFileSync(
-//             join(import.meta.dirname, `./schemas/${schemaFilename}`),
-//             'utf8'
-//           );
-//           const sitemapDoc = parseXml(sitemap);
-//           const schemaDoc = parseXml(schema);
-//           const isValid = sitemapDoc.validate(schemaDoc);
-
-//           if (!isValid) {
-//             throw new Error(
-//               `${sitemapFilename} is not valid: ${sitemapDoc.validationErrors}`
-//             );
-//           }
-
-//           expect(isValid).toBeTruthy();
-//         } catch (err) {
-//           // Throw error again to fail test if a sitemap cannot be parsed correctly
-//           throw new Error(err);
-//         }
-//       });
-//     });
-//   });
-
-//   describe('Posts sitemap', () => {
-//     describe('Last modified date', () => {
-//       test('All posts should have a valid last modified date', () => {
-//         const postsSitemap = readFileSync(
-//           join(distPath, 'sitemap-posts.xml'),
-//           'utf8'
-//         );
-//         const postsSitemapDoc = parseXml(postsSitemap);
-//         const postURLNodes = postsSitemapDoc
-//           .root()
-//           .childNodes()
-//           .filter(node => {
-//             return node.name() === 'url';
-//           });
-//         const lastmodDates = postURLNodes
-//           .map(node => {
-//             return node
-//               .childNodes()
-//               .filter(node => node.name() === 'lastmod')
-//               .map(node => node.text());
-//           })
-//           .flat();
-
-//         // Ensure all posts have a last modified date
-//         expect(lastmodDates).toHaveLength(postURLNodes.length);
-
-//         // Ensure all last modified dates are valid dates
-//         lastmodDates.forEach(date => {
-//           expect(new Date(date)).toBeInstanceOf(Date);
-//         });
-//       });
-//     });
-//   });
-// });
