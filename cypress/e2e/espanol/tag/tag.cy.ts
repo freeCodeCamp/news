4d90887453a97a7a26eaffbe2a23605a04d9e7de
Kristofer Koishigawa <2051070+scissorsneedfoodtoo@users.noreply.github.com>
2026-05-04 17:29:42 +1200

    feat: add and update tests for spanish / ghost sourced author and tag pages

--- cypress/e2e/espanol/tag/tag.cy.ts
+++ cypress/e2e/espanol/tag/tag.cy.ts
@@ -2,7 +2,13 @@ import { loadAndCountAllPostCards } from '../../../support/utils/post-cards';
 
 const selectors = {
   tagName: "[data-test-label='tag-name']",
-  tagPostCount: "[data-test-label='tag-post-count']"
+  tagPostCount: "[data-test-label='tag-post-count']",
+  socialMedia: {
+    rss: {
+      link: "[data-test-label='rss-link']",
+      icon: "[data-test-label='rss-icon']"
+    }
+  }
 };
 
 describe('Tag pages (Ghost sourced)', () => {
@@ -15,11 +21,27 @@ describe('Tag pages (Ghost sourced)', () => {
     cy.visit('/tag/javascript/');
   });
 
-  it('should render', () => {
-    cy.contains(selectors.tagName, '#JAVASCRIPT');
+  context('General tests', () => {
+    it('should render', () => {
+      cy.contains(selectors.tagName, '#JAVASCRIPT');
+    });
+
+    it('the number of total posts should match the post count at the top of the page (4)', () => {
+      loadAndCountAllPostCards(selectors.tagPostCount);
+    });
   });
 
-  it('the number of total posts should match the post count at the top of the page (4)', () => {
-    loadAndCountAllPostCards(selectors.tagPostCount);
+  // Note: All tags should have an RSS link and icon
+  context('RSS', () => {
+    it('should show an RSS link and icon with the expected attributes', () => {
+      cy.get(selectors.socialMedia.rss.link)
+        .should(
+          'have.attr',
+          'href',
+          'http://localhost:8080/espanol/news/tag/javascript/rss/'
+        )
+        .find('svg')
+        .should('have.attr', 'data-test-label', 'rss-icon');
+    });
   });
 });
