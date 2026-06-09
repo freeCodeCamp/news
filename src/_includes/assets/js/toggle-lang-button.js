7ad048d70e3f382b668ea2cced0108d97f6434b1
Anna Cottrill <a.rcottrill521@gmail.com>
2026-06-09 15:13:32 -0400

    [skip ci] - global icon and dropdown

---
+++ src/_includes/assets/js/toggle-lang-button.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document
+  .getElementById('toggle-lang-button')
+  .addEventListener('click', function () {
+    const dropDownMenu = document.getElementById('nav-lang-list');
+    const toggleButton = document.getElementById('toggle-lang-button');
+    dropDownMenu.classList.toggle('display-menu');
+    toggleButton.ariaExpanded =
+      toggleButton.ariaExpanded == 'true' ? 'false' : 'true';
+  });
+})
